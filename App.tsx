51b07a1d8897a2db3001b8d9a63bfa05409edac0
ajberamdas <ramdas.a@foxberry.in>
2023-07-06 11:08:02 +0530

    Solve location issue

--- App.tsx
+++ App.tsx
@@ -3,14 +3,13 @@ import {Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import BottamNavigation from './src/Navigation/BottamNavigation';
-import useLocation from './src/getLocation';
 
 export default function App() {
   // const {GetLocationHook} = useLocation();
-  const {GetLocationHook} = useLocation();
-  React.useEffect(() => {
-    GetLocationHook();
-  }, []);
+  // const {GetLocationHook} = useLocation();
+  // React.useEffect(() => {
+  //   GetLocationHook();
+  // }, []);
   return (
     <NavigationContainer>
       <BottamNavigation />
--- src/getLocation.js
+++ src/getLocation.js
@@ -3,49 +3,6 @@ import React, {useState} from 'react';
 import Geolocation from 'react-native-geolocation-service';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const useLocation = () => {
-  const requestLocationPermission = async () => {
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-        {
-          title: 'location permission',
-          message: 'location permission',
-          // buttonNeutral: 'Ask Me Later',
-          // buttonNegative: 'Cancel',
-          buttonPositive: 'yes',
-        },
-      );
-      console.log('granted', granted);
-      if (granted === 'granted') {
-        console.log('You can use Geolocation');
-        getGeoLocationPosition();
-        return true;
-      } else {
-        console.log('You cannot use Geolocation');
-        return false;
-      }
-    } catch (err) {
-      return false;
-    }
-  };
-
-  const GetLocationHook = () => {
-    const result = requestLocationPermission();
-
-    result.then(res => {
-      if (res) {
-        getGeoLocationPosition();
-      } else {
-        getGeoLocationPosition();
-        //Linking.openSettings();
-      }
-    });
-  };
-
-  return {
-    GetLocationHook,
-  };
-};
+const useLocation = () => {};
 
 export default useLocation;
